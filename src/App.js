abbbdd5faf433b0b4f560a9684de1debfedd9391
yunja <dev.yunja@gmail.com>
2021-08-14 23:08:48 +1000

    Example: fetch chat history

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [history, setHistory] = useState([]);
+
+  useEffect(() => {
+    fetchChatHistory();
+  }, []);
+
+  const fetchChatHistory = async () => {
+    const res = await axios('https://programming.coffee/history');
+    setHistory(res.data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {history.map((history, index) => <div key={index}>[{history.date}] {history.user}: {history.message}</div>)}
     </div>
   );
 }
