b3a89af2a00ef261f7340c2a80f3241b3d9b611a
MIKI <thiagoo@naver.com>
2021-08-16 18:15:44 +0900

    Fixed Load chat history

--- src/App.js
+++ src/App.js
@@ -2,29 +2,26 @@ import React, { Component} from 'react';
 import './App.css'; 
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import Data from './data';
 
   function App() {
-        const [history, setHistory] = useState([]);
+      const [history, setHistory] = useState([]);
+  
+      useEffect(() => {
+        fetchChatHistory();
+      }, []);
     
-        useEffect(() => {
-          fetchChatHistory();
-        }, []);
-      
-        const fetchChatHistory = async () => {
-          const res = await axios('https://programming.coffee/history');
-          setHistory(res.data);
-        };
+      const fetchChatHistory = async () => {
+        const res = await axios('https://programming.coffee/history');
+        setHistory(res.data);
+      };
       
     return(
 
       <div>
-        <Data historyProps={history}/>
+        {history.map((history, index) => <div key={index}>[{history.date}] {history.user}: {history.message}</div>)}
       </div>
-
     );
 
 }
-
 
 export default App;
