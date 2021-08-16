ab3c12bda9fa4e16e394b22ff0deee8fb8eb1b64
MIKI <thiagoo@naver.com>
2021-08-16 17:29:33 +0900

    Add Load chat content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from 'react';
+import './App.css'; 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Data from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  function App() {
+        const [history, setHistory] = useState([]);
+    
+        useEffect(() => {
+          fetchChatHistory();
+        }, []);
+      
+        const fetchChatHistory = async () => {
+          const res = await axios('https://programming.coffee/history');
+          setHistory(res.data);
+        };
+      
+    return(
+
+      <div>
+        <Data historyProps={history}/>
+      </div>
+
+    );
+
 }
+
 
 export default App;
