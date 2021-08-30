29b1ced8bc84279f1744150614377fc4bde548c7
MIKI <thiagoo@naver.com>
2021-08-30 20:58:17 +0900

    feat: Set router to handle pages - 프로젝트 진행을 위해 라우터 구조 설정. - 파일 업로드 하기 전(인트로 페이지), 파일 업로드 한 후(대시보드   페이지)로 페이지 구성함.

---
+++ src/component/ShowChatHistory.js
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+export default function ShowChatHistory(){
+    const [history, setHistory] = useState([]);
+
+      useEffect(() => {
+        fetchChatHistory();
+      }, []);
+    
+      const fetchChatHistory = async () => {
+        const res = await axios('https://programming.coffee/history');
+        setHistory(res.data);
+      };
+      
+    return (
+        <div>
+            {history.map((history, index) => <div key={index}>[{history.date}] {history.user}: {history.message}</div>)}
+        </div>
+    );
+}
