15945c1fa57c55a934f00aadc6a3ded2e75f8b87
chaechae66 <chaechae6326@gmail.com>
2021-10-01 21:14:04 +0900

    211001 pm 9

--- src/components/mentionRankCard/MentionRankCard.jsx
+++ src/components/mentionRankCard/MentionRankCard.jsx
@@ -1,21 +1,6 @@
 import { useEffect, useState } from 'react'
 import RankMember from '../rankMember/RankMember'
 
-// 오늘 2021-09-26
-// 일주일치 뽑아
-
-// const arr = [
-//   { date: '', user: '방구석코딩', mentionCount: 1 },
-//   { date: '2021-09-08', user: '방구석코딩', mentionCount: 1 },
-//   { date: '2021-09-07', user: '방구석코딩', mentionCount: 1 },
-//   { date: '2021-09-06', user: '방구석코딩', mentionCount: 1 },
-//   { date: '2021-09-05', user: '방구석코딩', mentionCount: 1 },
-//   { date: '2021-09-04', user: '방구석코딩', mentionCount: 1 },
-//   { date: '2021-09-03', user: '방구석코딩', mentionCount: 1 },
-// ]
-// const arr2 = [{ user: 'xxx', count: 0, rank: 1 }]
-// const totalCount = 0
-
 export default function MentionRankCard() {
   const [daily, setDaily] = useState([])
   const [weekly, setWeekly] = useState([])
@@ -36,57 +21,55 @@ export default function MentionRankCard() {
         setDaily(getData(data_1, 1))
         setWeekly(getData(data_1, 7))
         setMonthly(getData(data_1, 30))
+        // data 없을 때 처리
+
         return
       })
   }, [])
+
   console.log('daily', daily)
   console.log('weekly', weekly)
   console.log('monthly', monthly)
+
   function getData(data, days) {
+    const sumData = sumCount(getDataRewinded(data, days))
+    const sortedData = sortData(sumData)
+
+    console.log('sortedData', sortedData)
+    // return addRank(sortedData)
+  }
+
+  function getDataRewinded(data, days) {
     const rewindDaysTsp = today.getTime() - (days - 1) * 24 * 60 * 60 * 1000
 
     const todayString = dateTimestampToString(today)
     const dateKey =
       data[0].date === todayString
-        ? dateTimestampToString(rewindDaysTsp) // 40일 전 날짜 스트링, 8/18
-        : data[0].date // data 최상단 요소의 날짜 스트링, 9/9
+        ? dateTimestampToString(rewindDaysTsp)
+        : data[0].date
     const datekeyTsp = dateStringToTimestamp(dateKey) - 0
     const from = datekeyTsp - (days - 1) * 24 * 60 * 60 * 1000
     const newData = data.filter(ele => dateStringToTimestamp(ele.date) >= from)
-    // 데이터 날짜가 9/9로부터 -7일까지 나오도록
-    // 9.3 =< ele.data =< 9.9
-    const reducedData = newData.reduce(
-      (acc, curr) => {
-        if (acc.user === curr.user) {
-          acc.count += curr.mentionCount
-        } else if (acc.user !== curr.user) {
-          const idx = acc.findIndex(ele => ele.user === curr.user)
-          if (idx >= 0) {
-            acc[idx].count += curr.mentionCount
-          } else if (idx === -1) {
-            acc.push({ user: curr.user, count: curr.mentionCount })
-          }
-        }
-        return acc
-      },
-      [{ user: '', count: 0 }]
-    )
-    const slicedData = reducedData.slice(1)
 
-    const sortedData = slicedData.sort((a, b) => {
-      if (a.count < b.count) {
-        return 1
-      }
-      if (a.count > b.count) {
-        return -1
-      }
-      if (a.count === b.count) {
-        return 0
-      }
-    })
-
-    return sortedData
+    return newData
   }
+
+  function sumCount(data) {
+    return data.reduce((acc, curr) => {
+      acc[curr.user] = acc[curr.user]
+        ? acc[curr.user] + curr.mentionCount
+        : curr.mentionCount
+      return acc
+    }, {})
+  }
+
+  function sortData(data) {
+    return Object.entries(data)
+      .sort((a, b) => b[1] - a[1])
+      .slice(0, 7)
+      .map((ele, idx) => ({ user: ele[0], count: ele[1], rank: idx + 1 }))
+  }
+
   function dateStringToTimestamp(str) {
     var y = str.substr(0, 4)
     var m = str.substr(5, 2)
@@ -108,10 +91,10 @@ export default function MentionRankCard() {
 
   return (
     <>
-      <RankMember
+      {/* <RankMember
         rankData={weekly !== [] && weekly}
-        detail={[{ key: 'mentionCount', postFix: '회' }]}
-      />
+        detail={[{ key: 'count', postFix: '회' }]}
+      /> */}
     </>
   )
 }
