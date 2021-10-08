2412d5dcc7e9e5914fed7df5ea1556922301bcd1
Bugeon <bearbu@naver.com>
2021-10-09 02:36:56 +0900

    style: intro page css apply

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -3,6 +3,9 @@ import { useHistory } from 'react-router-dom'
 
 import { PATHS } from '../Routes'
 
+import styles from './Intro.module.css'
+import introIMG from '../assets/kafuncha-Intro.png'
+
 export default function Intro() {
   const inputRef = useRef(null)
   const history = useHistory()
@@ -31,12 +34,31 @@ export default function Intro() {
   }
 
   return (
-    <div>
-      <div>업로드</div>
+    <div className={styles.intro}>
       <div>
-        <input ref={inputRef} type="file" accept=".csv" />
-        <button onClick={uploadFile}>업로드</button>
+        <h1>
+          Kafuncha로 <span className={styles.dot}>편리하게</span>
+          <br />
+          <span className={styles['half-highlight']}>잠수 관리</span> 하세요!
+        </h1>
+        <p>
+          데이터 조회를 위해 PC 카카오톡에서 내려받은 csv파일을 업로드 해주세요
+          :)
+          <br />
+          채팅 횟수, 랭킹, 잠수 인원 등을 확인하실 수 있습니다.
+        </p>
+        <div className={styles.fileBox}>
+          <label htmlFor="input_file">파일 업로드</label>
+          <input
+            ref={inputRef}
+            type="file"
+            accept=".csv"
+            id="input_file"
+            onChange={uploadFile}
+          />
+        </div>
       </div>
+      <img src={introIMG} alt="kafuncha" />
     </div>
   )
 }
